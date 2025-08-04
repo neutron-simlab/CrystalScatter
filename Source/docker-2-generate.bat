b6dace02e76d8e1ac862bfaa6823683182f141d5
Michael Wagener <m.wagener@fz-juelich.de>
2025-08-04 13:34:12 +0200

    Update docker generation with python

--- Source/docker-2-generate.bat
+++ Source/docker-2-generate.bat
@@ -1,7 +1,7 @@
 @echo on
 
 rem *** prepare the runner image
-docker run -t --name scattercons-tmp  -v "%cd%:/home/user/project" g76r/qt6-runner:qt-6.8.3-debug sh -c "apt install -y libxkbcommon-dev libgl-dev; cp /home/user/project/build/sas_scatter2Cons /bin"
+docker run -t --name scattercons-tmp  -v "%cd%:/home/user/project" g76r/qt6-runner:qt-6.8.3-debug sh -c "apt-get update; apt install -y libxkbcommon-dev libgl-dev python3; cp /home/user/project/build/sas_scatter2Cons /bin"
 
 rem *** remove the 'old' destination image
 docker image rm crystalscattercons-run
